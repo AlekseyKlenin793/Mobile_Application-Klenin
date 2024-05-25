3d2d49d757ee4fd2668dc54733edd8e6ecd86236
Aleksey Klenin <kleninalex.793@gmail.com>
2024-05-25 04:05:56 +0300

    Changed Lesson_6 functionality for MireaProject

--- Lesson_3 + MireaProject/mireaproject/src/main/java/ru/mirea/kleninaa/mireaproject/FileOperationsFragment.java
+++ Lesson_3 + MireaProject/mireaproject/src/main/java/ru/mirea/kleninaa/mireaproject/FileOperationsFragment.java
@@ -2,12 +2,17 @@ package ru.mirea.kleninaa.mireaproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +25,9 @@ public class FileOperationsFragment extends Fragment {
 
     private ImageView imageView;
     private FloatingActionButton fab;
+    private Button brightnessButton;
+
+    private Bitmap originalBitmap;
 
     @Nullable
     @Override
@@ -28,11 +36,21 @@ public class FileOperationsFragment extends Fragment {
 
         imageView = view.findViewById(R.id.imageView);
         fab = view.findViewById(R.id.fab);
+        brightnessButton = view.findViewById(R.id.brightnessButton);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openFileChooser();
+            }
+        });
+
+        brightnessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (originalBitmap != null) {
+                    adjustBrightness(originalBitmap, 0.5f);
+                }
             }
         });
 
@@ -51,8 +69,30 @@ public class FileOperationsFragment extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
             Uri imageUri = data.getData();
-            imageView.setImageURI(imageUri);
-            imageView.setVisibility(View.VISIBLE);
+            try {
+                originalBitmap = MediaStore.Images.Media.getBitmap(requireActivity().getContentResolver(), imageUri);
+                imageView.setImageBitmap(originalBitmap);
+                imageView.setVisibility(View.VISIBLE);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private void adjustBrightness(Bitmap bitmap, float brightness) {
+        ColorMatrix matrix = new ColorMatrix();
+        matrix.set(new float[] {
+                brightness, 0, 0, 0, 0,
+                0, brightness, 0, 0, 0,
+                0, 0, brightness, 0, 0,
+                0, 0, 0, 1, 0
+        });
+
+        Bitmap adjustedBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), bitmap.getConfig());
+        android.graphics.Canvas canvas = new android.graphics.Canvas(adjustedBitmap);
+        android.graphics.Paint paint = new android.graphics.Paint();
+        paint.setColorFilter(new ColorMatrixColorFilter(matrix));
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+        imageView.setImageBitmap(adjustedBitmap);
     }
 }
